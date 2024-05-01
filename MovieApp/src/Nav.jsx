68a2d8bc8c9f05274c5219875d0d0fdb13cf5a17
alex <1alexispetropoulos@gmail.com>
2024-05-01 22:08:29 +0200

    responsive navbar

--- MovieApp/src/Nav.jsx
+++ MovieApp/src/Nav.jsx
@@ -11,13 +11,13 @@ export default function Nav() {
 
     return (
             <nav className="text-white">
-                <div className="hidden md:visible">
+                <div className="hidden md:flex">
                     <a href="#Home" className="mr-2">Home</a>
-                    <a href="Movies" className="mr-2">Movies</a>
-                    <a href="Tv shows" className="">Tv shows</a>
+                    <a href="#Movies" className="mr-2">Movies</a>
+                    <a href="#Tvshows" className="">Tv shows</a>
                 </div>
 
-                <div className="flex items-center">
+                <div className="md:hidden flex items-center">
                     <button onClick={toggleNavbar}>{isOpen ? <CloseMenu /> : <MenuIcon /> }</button>
                 </div>
             </nav>
--- MovieApp/src/ResponsiveNav.jsx
+++ MovieApp/src/ResponsiveNav.jsx
@@ -1,11 +1,18 @@
 export function MenuIcon() {
-    return(
-        <img className="w-6 h-6" src="src/assets/menuicon.png"/>
+    return (
+        <img className="w-6 h-6" src="src/assets/menuicon.png" />
     );
 }
 
 export function CloseMenu() {
-    return(
-        <img className="w-6 h-6" src="src/assets/closemenu.png"/>
+    return (
+        <>
+            <img className="w-6 h-6" src="src/assets/closemenu.png" />
+            <div className="flex flex-col items-center flex-wrap">
+                <a href="#Home" className="mr-2">Home</a>
+                <a href="#Movies" className="mr-2">Movies</a>
+                <a href="#Tvshows" className="">Tv shows</a>
+            </div>
+        </>
     );
 }
