51c450f645aba60ee79b1ab334aabe0e2080e82f
alex <1alexispetropoulos@gmail.com>
2024-05-02 21:28:14 +0200

    composant recherche

--- MovieApp/src/Search.jsx
+++ MovieApp/src/Search.jsx
@@ -41,20 +41,19 @@ export default function SearchMovie() {
                 </div>
             </form>
             <div className="bg-[#1a1820]">
-                <div className="mx-6 my-6">
+                <div className="mx-6 my-6 lg:mx-20">
                     <p className="text-xl font-bold text-red-600">Results for</p>
                     <h2 className="text-4xl font-bold text-white">{query}</h2>
                 </div>
-                <div className="flex flex-col mx-6 text-white">
-
+                <div className="flex grid flex-col mx-6 text-white lg:grid-cols-6 lg:gap-5 lg:mx-20">
                     {movies.map(movie => (
                         <div key={movie.id} className="flex items-center justify-center my-4">
                             <div className="">
                                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="rounded-lg" />
-                                <p className="mt-4 text-2xl font-bold">{movie.title.length > 20 ? `${movie.title.substring(0, 25)}...` : movie.title}</p>
-                                <div className="flex items-center justify-between mt-2">
-                                    <p className="text-xl font-bold">⭐ {movie.vote_average}</p>
-                                    <p className="flex px-2 py-1 font-bold bg-gray-700 rounded">{new Date(movie.release_date).getFullYear()}</p>
+                                <p className="mt-4 text-2xl font-bold lg:text-sm">{movie.title.length > 20 ? `${movie.title.substring(0, 25)}...` : movie.title}</p>
+                                <div className="flex items-center justify-center justify-between mt-2">
+                                    <p className="text-lg font-bold lg:text-base">⭐ {movie.vote_average > 3 ? `${movie.vote_average.toFixed(1)}` : movie.vote_average}</p>
+                                    <p className="flex px-2 py-1 font-bold bg-gray-700 rounded lg:text-base">{new Date(movie.release_date).getFullYear()}</p>
                                 </div>
                             </div>
                         </div>
