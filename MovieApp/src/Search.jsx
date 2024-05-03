4ff39230c27d00238b30f6ae6dbf143f3f3a74ac
alex <1alexispetropoulos@gmail.com>
2024-05-03 12:03:00 +0200

    gestion des absences de données de l'api

--- MovieApp/src/Search.jsx
+++ MovieApp/src/Search.jsx
@@ -48,12 +48,12 @@ export default function SearchMovie() {
                 <div className="flex grid flex-col mx-6 text-white lg:grid-cols-6 lg:gap-5 lg:mx-20">
                     {movies.map(movie => (
                         <div key={movie.id} className="flex items-center justify-center my-4">
-                            <div className="">
-                                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="rounded-lg" />
+                            <div className="h-full w-full flex flex-col">
+                                <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'src/assets/poster_unavailable.jpg'} alt={movie.title} className="rounded-lg bg-gray-600 w-auto h-auto" />
                                 <p className="mt-4 text-2xl font-bold lg:text-sm">{movie.title.length > 20 ? `${movie.title.substring(0, 25)}...` : movie.title}</p>
                                 <div className="flex items-center justify-center justify-between mt-2">
                                     <p className="text-lg font-bold lg:text-base">⭐ {movie.vote_average > 3 ? `${movie.vote_average.toFixed(1)}` : movie.vote_average}</p>
-                                    <p className="flex px-2 py-1 font-bold bg-gray-700 rounded lg:text-base">{new Date(movie.release_date).getFullYear()}</p>
+                                    <p className="flex px-2 py-1 font-bold bg-gray-700 rounded lg:text-base">{movie.release_date ? new Date(movie.release_date).getFullYear() : "?"}</p>
                                 </div>
                             </div>
                         </div>
