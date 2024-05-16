35d565d3db0dde36d7896bd140a7314ab132f818
alex <1alexispetropoulos@gmail.com>
2024-05-16 14:32:43 +0200

    ajout reals et acteurs

--- MovieApp/src/Search/DisplaySearchDetails.jsx
+++ MovieApp/src/Search/DisplaySearchDetails.jsx
@@ -11,7 +11,7 @@ export default function DisplaySearchDetails() {
     useEffect(() => {
         const fetchMovieDetails = async () => {
             try {
-                const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=e0fa0c69b5ec0c1fd6153567c8a33701`);
+                const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=e0fa0c69b5ec0c1fd6153567c8a33701&append_to_response=casts,videos,images,releases`);
                 const data = await response.json();
                 setMovie(data);
             } catch (error) {
@@ -35,13 +35,29 @@ export default function DisplaySearchDetails() {
                         <div className="mx-6">
                             <h2 className="text-3xl font-bold text-white">{movie.title}</h2>
                             <ul className="flex items-center mt-2 text-white">
-                                <li className="mr-4 text-lg font-bold lg:text-base">⭐ {movie.vote_average > 3 ? `${movie.vote_average.toFixed(1)}` : movie.vote_average} -</li>
+                                <li className="mr-4 text-lg font-bold lg:text-base">⭐ {movie.vote_average > 3 ? `${movie.vote_average.toFixed(1)}` : movie.vote_average} </li>
                                 <li className="mr-4 text-lg font-bold lg:text-base">{movie.runtime}m</li>
                                 <li className="text-lg font-bold lg:text-base">{movie.release_date ? new Date(movie.release_date).getFullYear() : "?"}</li>
                             </ul>
                             <ul className="flex text-white">{movie.genres.map(genre => (
                                 <li key={genre.id}>{genre.name} </li>
                             ))}
+                            </ul>
+                            <ul className="text-white flex">
+                                {movie.casts.cast
+                                .filter(cast => cast.known_for_department === "Acting")
+                                .slice(0, 9)
+                                .map(cast => (
+                                    <li key={cast.id} className="mr-2">{cast.name}</li>
+                                ))}
+                            </ul>
+                            <ul className="text-white flex">
+                                {movie.casts.crew
+                                .filter(crew => crew.department === "Directing")
+                                .slice(0,2)
+                                .map(crew => (
+                                    <li key={crew.id} className="mr-2">{crew.original_name}</li>
+                                ))}
                             </ul>
                             <p className="mt-4 text-white">{movie.overview}</p>
                         </div>
