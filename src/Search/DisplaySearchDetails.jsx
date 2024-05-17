63b8df2efe6389b9711eff40eb36254f3f23421f
alex <1alexispetropoulos@gmail.com>
2024-05-17 16:51:31 +0200

    ajout de virgules pour directors & cast

--- src/Search/DisplaySearchDetails.jsx
+++ src/Search/DisplaySearchDetails.jsx
@@ -24,42 +24,47 @@ export default function DisplaySearchDetails() {
 
     return (
         <>
-            <div className="bg-[#1a1820]">
+            <div className="bg-[#1a1820] h-full">
                 <Header />
                 <SearchBar />
                 {movie ? (
-                    <div className="flex flex-col pb-12 lg:flex">
+                    <div className="flex flex-col pb-12 lg:flex-row lg:mx-20">
                         <div className="flex items-center justify-center">
-                            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : '/src/assets/poster_unavailable.jpg'} alt={movie.title} className="bg-gray-600 rounded-lg w-[90%] h-[90%] mb-6" />
+                            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : '/src/assets/poster_unavailable.jpg'} alt={movie.title} className="bg-gray-600 rounded-lg w-[90%] h-[90%] mb-6 lg:w-full lg:h-full" />
                         </div>
-                        <div className="mx-6">
+                        <div className="mx-6 lg:mt-6">
                             <h2 className="text-3xl font-bold text-white">{movie.title}</h2>
                             <ul className="flex items-center mt-2 text-white">
                                 <li className="mr-4 text-lg font-bold lg:text-base">⭐ {movie.vote_average > 3 ? `${movie.vote_average.toFixed(1)}` : movie.vote_average} </li>
                                 <li className="mr-4 text-lg font-bold lg:text-base">{movie.runtime}m</li>
                                 <li className="text-lg font-bold lg:text-base">{movie.release_date ? new Date(movie.release_date).getFullYear() : "?"}</li>
                             </ul>
-                            <ul className="flex text-gray-400 mt-1">{movie.genres.map(genre => (
-                                <li key={genre.id} className="text-white">{genre.name}, </li>
-                            ))}
+                            <ul className="flex text-gray-400 mt-1">
+                                <li className="text-white">
+                                    {movie.genres
+                                        .map(genre => genre.name)
+                                        .join(', ')}
+                                </li>
+                            </ul>
+                            <ul className="text-white font-bold mt-2">
+                                <span className="text-gray-400">Directed by </span>
+                                <li className="mr-2 inline">
+                                    {movie.casts.crew
+                                        .filter(crew => crew.job === "Director")
+                                        .slice(0, 2)
+                                        .map(crew => crew.original_name)
+                                        .join(', ')}
+                                </li>
                             </ul>
                             <ul className="text-white mt-2 font-bold">
-                            <span className="text-gray-400 items-start">Starring </span>
-                                {movie.casts.cast
-                                .filter(cast => cast.known_for_department === "Acting")
-                                .slice(0, 9)
-                                .map(cast => (
-                                    <li key={cast.id} className="mr-2 inline">{cast.name}</li>
-                                ))}
-                            </ul>
-                            <ul className="text-white font-bold">
-                            <span className="text-gray-400">Directed by </span>
-                                {movie.casts.crew
-                                .filter(crew => crew.job === "Director")
-                                .slice(0,2)
-                                .map(crew => (
-                                    <li key={crew.id} className="inline">{crew.original_name}</li>
-                                ))}
+                                <span className="text-gray-400 items-start">Starring </span>
+                                <li className="mr-2 inline">
+                                    {movie.casts.cast
+                                        .filter(cast => cast.known_for_department === "Acting")
+                                        .slice(0, 9)
+                                        .map(cast => cast.name)
+                                        .join(', ')}
+                                </li>
                             </ul>
                             <p className="mt-4 text-white">{movie.overview}</p>
                         </div>
